3bec3516763a58be0a8e5e74a7f3dba75747e1e3
BaltasisKos <Baltasiskostas@hotmail.com>
2025-04-23 19:14:31 +0300

    Componentes input with diff steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PersonTableComponent } from './components/person-table/person-table.component';
+import { Person } from './shared./interfaces/person';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PersonTableComponent],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   name = 'Kostas';
+  
 
+  // Step 1: One way binding of data
   person = {
     givenName: 'Kostas',
     surName: 'Kostakis',
     age: 35,
     email: 'kostakis@aueb.gr'
+  }
+
+  // Step 3: Component input
+  person0: Person ={
+    givenName: "Christodoulos",
+    surName: "Fragkoudakis",
+    age: 55,
+    email: "chfraf@aueb.gr",
+    address: "Athens, Greece"
+  }
+
+  person1: Person = {
+    givenName: "John",
+    surName: "Doe",
+    age: 32,
+    email: "john@example.com",
+    address: 'New York, USA'
   }
 }
