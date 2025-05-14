51a28e3c72694ddc3a33a9163b5f8ea34c7a2f55
BaltasisKos <Baltasiskostas@hotmail.com>
2025-05-14 21:22:22 +0300

    google-login

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -2,11 +2,12 @@ import { CanActivateFn, Router } from '@angular/router';
 import {inject} from '@angular/core';
 import { UserService } from '../services/user.service';
 
+
 export const authGuard: CanActivateFn = (route, state) => {
   const userService = inject(UserService);
   const router = inject(Router);
   
-  if (userService.user$()) {
+  if (userService.user$() && !userService.isTokenExpired()) {
     return true;
   }
 
